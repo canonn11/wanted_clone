fb1d6b405b89b04f8b9f09c8be45e2070e71a3e7
canonn11 <myungkwanokk@gmail.com>
2022-07-04 22:50:15 +0900

    로그인 페이지 구현

---
+++ src/routes/user/login.svelte
@@ -0,0 +1,121 @@
+<button class="pt-3 pl-3">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-x"
+  >
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
+  </svg>
+</button>
+
+<div class="text-center mt-12">
+  <h1 class="text-2xl font-black">
+    직장인을 위한 <br />
+    커리어 플랫폼, 핑거!
+  </h1>
+
+  <div class="mt-4 text-sm">
+    커리어 성장과 행복을 위한 여정 <br />
+    지금 핑거에서 시작하세요.
+  </div>
+</div>
+
+<div class="p-4 mt-9">
+  <input
+    type="text"
+    class="w-full h-12 border border-gray-300 rounded font-bold text-sm placeholder-neutral-400"
+    placeholder=" 이메일"
+  />
+</div>
+
+<div class="px-4">
+  <a
+    href="/users/signup"
+    class="block text-center w-full bg-gray-400 rounded-full h-12 text-gray-300 font-bold flex items-center justify-center"
+    >이메일로 계속하기
+  </a>
+  <div class="my-2 text-sm text-gray-300 text-center">또는</div>
+  <a
+    href="/kakao"
+    class="block text-center w-full bg-yellow-300 rounded-full h-12 text-black font-bold flex items-center "
+  >
+    <span class="ml-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-message-circle"
+        ><path
+          d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+        />
+      </svg>
+    </span>
+    <span class="flex-1 mr-4">Kakao로 계속하기 </span>
+  </a>
+  <a
+    href="/facebook"
+    class="block text-center mt-3 w-full bg-blue-500 rounded-full h-12 text-white  flex items-center "
+  >
+    <span class="ml-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-facebook"
+        ><path
+          d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+        /></svg
+      >
+    </span>
+    <span class="flex-1 mr-3">페이스북으로 계속하기 </span>
+  </a>
+  <a
+    href="/google"
+    class="block  text-center mt-3 w-full bg-white border border-gray-300 rounded-full h-12 text-gray-500 font-bold  flex items-center "
+  >
+    <span class="ml-4">
+      <img
+        src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/c37230e9-14c7-4124-9d64-b4302201d6ec.png"
+        class="h-5 w-5"
+      />
+    </span>
+    <span class="flex-1 mr-4">Google로 계속하기 </span>
+  </a>
+</div>
+
+<div class="mt-4 text-xs px-4 text-center">
+  걱정마세요! 여러분의 지원 활동은 페이스북에 노출되지 않습니다. 회원가입시
+  <a href="/policy/personal" class="text-blue-600">개인정보 처리방침</a>
+  을 읽었으며,
+  <a href="/policy/terms" class="text-blue-600">이용약관</a>
+  에 동의하신 것으로 간주합니다.
+</div>
+
+<div class="flex justify-center">
+  <div class="absolute bottom-0 mb-8 flex gap-2">
+    <div class="w-2 h-2 bg-neutral-300 rounded-full" />
+    <div class="w-2 h-2 bg-neutral-300 rounded-full" />
+    <div class="w-2 h-2 bg-neutral-300 rounded-full" />
+    <div class="w-2 h-2 bg-blue-500 rounded-full" />
+  </div>
+</div>
