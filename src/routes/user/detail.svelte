a25b5d81d9dddc2173ca2614a01d32292dd97dc6
canonn11 <myungkwanokk@gmail.com>
2022-07-05 23:33:38 +0900

    전문분야 설정 페이지 구현

---
+++ src/routes/user/detail.svelte
@@ -0,0 +1,71 @@
+<div class=" font-black p-4  h-12 text-center bg-white ">전문분야 설정</div>
+
+<div class="grid grid-cols-3 mt-2">
+  <span class="w-full border border-blue-500" />
+  <span class="w-full border border-gray-200" />
+  <span class="w-full border border-gray-200" />
+</div>
+
+<div class="flex gap-1 px-5 mt-5">
+  <button class="px-2 h-6 rounded-full bg-blue-500 text-white text-sm">
+    1
+  </button>
+  <button
+    class="px-2 h-6 border rounded-full bg-gray-200 text-gray-300 text-sm"
+  >
+    2
+  </button>
+  <button
+    class="px-2 h-6 border rounded-full bg-gray-200 text-gray-300 text-sm"
+  >
+    3
+  </button>
+</div>
+
+<div class=" p-4 font-black">
+  어떤 일을 하고 계신가요?<br />
+  딱 맞는 회사와 콘텐츠를 추천해 드려요!
+</div>
+
+<label for="" class="px-4 mt-3 text-sm font-bold block text-gray-500 mb-2"
+  >직군</label
+>
+<div class="flex justify-center px-4">
+  <select class="h-14 rounded-md w-full border border-gray-300 text-gray-500">
+    <option selected>선택해 주세요.</option>
+  </select>
+</div>
+
+<div class=" px-4 mt-3 text-sm text-gray-400 font-bold">
+  신입의 경우, 희망 직군을 선택해 주세요.
+</div>
+
+<label for="" class="px-4 mt-5 text-sm font-bold block text-gray-500 mb-2"
+  >경력</label
+>
+<div class="flex justify-center px-4">
+  <select class="h-14 rounded-md w-full border border-gray-300 text-gray-500">
+    <option selected>선택해 주세요.</option>
+  </select>
+</div>
+
+<div class="flex justify-between items-center mt-8 text-sm form-check px-4">
+  <div>
+    <input
+      class="h-5 w-5 mr-2 border-2 border-gray-400 rounded-md bg-white checked:bg-gray-800 checked:border-gray-800 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+      type="checkbox"
+      value=""
+      id="agree1"
+    />
+    <label class="inline-block text-gray-400 font-bold" for="agree1">
+      이벤트 소식 등 알림 정보 수신거부
+    </label>
+  </div>
+  <a href="/clause" class="text-gray-500 text-xs mr-3">자세히</a>
+</div>
+<div class="fixed bottom-0 left-0 right-0 mx-4 pb-4 pt-5 bg-white ">
+  <button
+    class="block text-center w-full rounded-full bg-gray-200 rounded h-10 text-sm font-black text-gray-600 flex items-center justify-center"
+    >다음</button
+  >
+</div>
