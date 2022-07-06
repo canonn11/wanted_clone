c6373c9f698c0c946982a713968f1209fa4731b5
canonn11 <myungkwanokk@gmail.com>
2022-07-06 22:58:32 +0900

    출석 이벤트 페이지 구현

---
+++ src/routes/events/attendance.svelte
@@ -0,0 +1,208 @@
+<div class="sticky top-0 z-10 w-full">
+  <div class=" font-black  p-4 h-12  text-sm text-center bg-white border-b ">
+    매일 출첵하고 선물받기!
+  </div>
+  <button class="mt-4 pl-3 absolute top-0 right-4">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="gray"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-x"
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  </button>
+</div>
+<div
+  class="flex gap-2 font-bold text-xs justify-center pt-12 items-center  text-gray-500"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-calendar"
+  >
+    <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+    <line x1="16" y1="2" x2="16" y2="6" />
+    <line x1="8" y1="2" x2="8" y2="6" />
+    <line x1="3" y1="10" x2="21" y2="10" />
+  </svg>
+  <span>DAY2</span>
+
+  <span class="border-l pl-2">오늘의 출첵 미션</span>
+</div>
+
+<div class=" font-black  mt-6  text-lg text-center ">
+  채용 공고 3개 열어보기
+</div>
+<div class=" mt-2  text-xs text-center ">
+  요즘 회사가 원하는 인재는? 공고에서 확인해보세요.
+</div>
+<div class="flex justify-center gap-1 mt-2 text-xs text-center">
+  <div class="text-gray-600">남은 시간</div>
+  <div class="text-red-500 font-bold ">18:04:38</div>
+</div>
+
+<div
+  class=" font-black  mt-8  justify-center gap-2 text-xs text-blue-500 items-center flex border-b border-t p-2 bg-blue-100"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="28"
+    height="28"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-gift"
+  >
+    <polyline points="20 12 20 22 4 22 4 12" />
+    <rect x="2" y="7" width="20" height="5" />
+    <line x1="12" y1="22" x2="12" y2="7" />
+    <path d="M12 7H7.5a2.5 2.5 0 0 1 0-5C11 2 12 7 12 7z" />
+    <path d="M12 7h4.5a2.5 2.5 0 0 0 0-5C13 2 12 7 12 7z" />
+  </svg>모든 출첵 미션 달성 시 총 27,000P가 지급됩니다
+</div>
+<div class="bg-gray-100">
+  <div class="p-2" />
+  <div class="bg-white rounded-md px-2 py-4 mx-3 ">
+    <div class="font-black pb-4 pl-4">나의 출첵 현황</div>
+    <div class="flex justify-between py-2">
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="80"
+          height="80"
+          viewBox="0 0 24 24"
+          fill="gray"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-frown"
+        >
+          <circle cx="12" cy="12" r="10" />
+          <path d="M16 16s-1.5-2-4-2-4 2-4 2" />
+          <line x1="9" y1="9" x2="9.01" y2="9" />
+          <line x1="15" y1="9" x2="15.01" y2="9" />
+        </svg>
+        <div class="absolute text-xs text-white font">미션 실패</div>
+        <div class="font-bold p-1">DAY 1</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center">
+        <div
+          class="flex flex-col justify-center items-center text-xs text-white rounded-full bg-blue-500 w-20 h-20"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-check-circle"
+          >
+            <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+            <polyline points="22 4 12 14.01 9 11.01" />
+          </svg>
+          <div class="font-bold ">1,000P</div>
+          <div>지급완료</div>
+        </div>
+        <div class="font-bold p-1">DAY 2</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">5,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 3</div>
+      </div>
+    </div>
+    <div class="flex justify-between py-2">
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">2,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 4</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">2,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 5</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">5,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 6</div>
+      </div>
+    </div>
+    <div class="flex justify-between py-2">
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">3,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 7</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">3,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 8</div>
+      </div>
+      <div class="flex flex-col w-1/3 justify-center items-center ">
+        <div
+          class="flex  justify-center items-center text- text-white rounded-full bg-gray-200 w-20 h-20"
+        >
+          <div class="font-bold ">5,000P</div>
+        </div>
+        <div class="font-bold p-1">DAY 9</div>
+      </div>
+    </div>
+  </div>
+  <div class="p-2" />
+  <div class="mx-4 pt-4 border-t border-white pb-20 ">
+    <div class="text-sm">알아두세요!</div>
+    <div class="text-xs">
+      * 출첵 미션은 앱에서만 참여 가능합니다.<br />
+      * 포인트는 무제한 커리어 콘텐츠인 핑거 플러스 구독 시 사용할 수 있습니다.
+    </div>
+  </div>
+</div>
+
+<div class="fixed z-10 bottom-0 left-0 right-0 px-4 pb-4 bg-gray-100">
+  <button
+    class="block text-center w-full rounded-full font-black bg-blue-500 rounded h-10 text-sm text-white flex items-center justify-center"
+    >오늘의 출첵 미션 참여하기</button
+  >
+</div>
