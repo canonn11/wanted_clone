bff88f1e796d3c724c9ee9e8f230509c4e9a2e97
canonn11 <myungkwanokk@gmail.com>
2022-07-06 22:59:35 +0900

    커리어 성장 페이지 구현

---
+++ src/routes/career/contents.svelte
@@ -0,0 +1,212 @@
+<div class="sticky z-10 top-0 bg-white flex justify-between">
+  <div class="flex justify-center items-center gap-4 p-4">
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12" />
+        <polyline points="12 19 5 12 12 5" />
+      </svg></a
+    >
+    <div class=" font-black  text-center bg-white">커리어 성장</div>
+  </div>
+  <a href="/" class="text-blue-500 text-sm p-4"> MY 영상 </a>
+</div>
+
+<img
+  class="w-full"
+  src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/993bf3f4-79d9-4060-879c-f93a58372f45.PNG"
+/>
+
+<div class="bg-white flex relative justify-between items-center py-4 px-2">
+  <select class="h-14 rounded-md w-2/5 text-black font-bold border-none">
+    <option selected>전체 태그</option>
+  </select>
+  <div class="flex gap-2">
+    <a
+      href="/"
+      class=" flex items-center justify-center border rounded-full h-10 w-10"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-align-justify"
+      >
+        <line x1="21" y1="10" x2="3" y2="10" />
+        <line x1="21" y1="6" x2="3" y2="6" />
+        <line x1="21" y1="14" x2="3" y2="14" />
+        <line x1="21" y1="18" x2="3" y2="18" />
+      </svg>
+      <div
+        class="flex absolute top-4 right-14 items-center justify-center text-sm  rounded-full border w-6 h-6 bg-blue-600 text-white"
+      >
+        2
+      </div>
+    </a>
+    <a
+      href="/"
+      class="flex items-center justify-center mr-2 border rounded-full w-10 h-10 bg-blue-600"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bell"
+      >
+        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+        <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+      </svg>
+    </a>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 bg-white">
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">롱런하는 마케터의 커리어 개발</div>
+        <div class="text-gray-500 text-xs ">2022.06.30 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#마케팅·광고 #브랜딩 #...</div>
+      </div>
+    </a>
+  </div>
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDh8fGZhc2hpb258ZW58MHx8fHwxNjQ0NzQxNTkx&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">
+          스타트업의 체계 없음이 가장 무서웠죠!
+        </div>
+        <div class="text-gray-500 text-xs ">2022.06.29 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#경영·비즈니스 #취업/...</div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 pt-8 px-4 bg-white">
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">롱런하는 마케터의 커리어 개발</div>
+        <div class="text-gray-500 text-xs ">2022.06.30 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#마케팅·광고 #브랜딩 #...</div>
+      </div>
+    </a>
+  </div>
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDh8fGZhc2hpb258ZW58MHx8fHwxNjQ0NzQxNTkx&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">
+          스타트업의 체계 없음이 가장 무서웠죠!
+        </div>
+        <div class="text-gray-500 text-xs ">2022.06.29 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#경영·비즈니스 #취업/...</div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 pt-8 px-4 bg-white">
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">롱런하는 마케터의 커리어 개발</div>
+        <div class="text-gray-500 text-xs ">2022.06.30 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#마케팅·광고 #브랜딩 #...</div>
+      </div>
+    </a>
+  </div>
+  <div>
+    <a href="/" class="">
+      <img
+        class="rounded-md w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDh8fGZhc2hpb258ZW58MHx8fHwxNjQ0NzQxNTkx&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="p-2">
+        <a
+          href="/"
+          class="text-yellow-300 text-xs border border-yellow-300 rounded-md font-bold text-center p-1"
+        >
+          아티클
+        </a>
+        <div class="mt-2 font-bold h-32">
+          스타트업의 체계 없음이 가장 무서웠죠!
+        </div>
+        <div class="text-gray-500 text-xs ">2022.06.29 (목) ~ 202...</div>
+        <div class="text-xs mt-2">#경영·비즈니스 #취업/...</div>
+      </div>
+    </a>
+  </div>
+</div>
