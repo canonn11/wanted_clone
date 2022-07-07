d544559a6e8de0db280140c0b5c6241d8cbd1ff0
canonn11 <myungkwanokk@gmail.com>
2022-07-07 18:33:06 +0900

    연봉 예상 페이지 구현

---
+++ src/routes/career/estimate.svelte
@@ -0,0 +1,279 @@
+<div class="sticky z-10 w-full top-0">
+  <div class="flex justify-between bg-purple-500 ">
+    <div class="flex justify-center items-center text-lg text-white gap-8 px-4">
+      <a href="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-arrow-left"
+        >
+          <line x1="19" y1="12" x2="5" y2="12" />
+          <polyline points="12 19 5 12 12 5" />
+        </svg></a
+      >
+      <div class=" font-black  text-center">예상연봉</div>
+    </div>
+    <a href="/" class="text-white text-sm p-3"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-vertical"
+      >
+        <circle cx="12" cy="12" r="1" />
+        <circle cx="12" cy="5" r="1" />
+        <circle cx="12" cy="19" r="1" />
+      </svg></a
+    >
+  </div>
+  <div class="flex bg-purple-500 pb-1">
+    <div class="flex gap-1 overflow-x-auto px-4">
+      <div class="whitespace-pre px-2 h-8 text-xs font-bold">ㅤㅤㅤ</div>
+      <button
+        class="whitespace-pre px-2 h-8 rounded-full text-xs font-bold  text-gray-300"
+      >
+        웹 개발자
+      </button>
+      <button
+        class="whitespace-pre px-2 h-8 rounded-full text-xs font-bold text-white border-2 border-slate-200"
+      >
+        서버 개발자
+      </button>
+      <button
+        class="whitespace-pre px-2 h-8 rounded-full text-xs font-bold text-gray-300"
+      >
+        프론트엔드 개발자
+      </button>
+      <button
+        class="whitespace-pre px-2 h-8 rounded-full text-xs font-bold text-gray-300"
+      >
+        소프트웨어 엔지니어
+      </button>
+    </div>
+  </div>
+</div>
+<div class="bg-purple-500 text-white text-center pt-6">
+  <div class="font-bold ">신입</div>
+  <div class="flex justify-center gap-2">
+    <div class="text-4xl font-black  ">3,513</div>
+    <div class="font-bold pt-5">만원</div>
+  </div>
+</div>
+
+<div class="relative">
+  <div class="flex gap-1 justify-center relative bg-purple-500 pt-4 ">
+    <div class="flex flex-col items-center  h-44 w-6 bg-purple-500">
+      <div class="h-3/6 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-violet-300" />
+      <button class="text-xs whitespace-pre text-white"> 신입 </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-2/5 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        1년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-16 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        2년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-14 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        3년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-12 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        4년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6 bg-purple-500">
+      <div class="h-10 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        5년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6 bg-purple-500">
+      <div class="h-9 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        6년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6 bg-purple-500">
+      <div class="h-8 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        7년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-7 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        8년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-6 w-5 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center text-xs whitespace-pre text-white">
+        9년
+      </button>
+    </div>
+    <div class="flex flex-col items-center h-44 w-6  bg-purple-500">
+      <div class="h-1 w-6 bg-purple-500" />
+      <button class="flex-1 w-5 bg-purple-400" />
+      <button class="flex justify-center  text-xs whitespace-pre text-white">
+        10년
+      </button>
+    </div>
+  </div>
+  <div
+    class="grid grid-rows-2 absolute bottom-20 left-6  items-center justify-center"
+  >
+    <div class="text-xs text-white font-bold ">3,513</div>
+    <div class="flex items-center justify-center p-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        height="12"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-circle"
+      >
+        <circle cx="12" cy="12" r="10" />
+      </svg>
+    </div>
+  </div>
+</div>
+
+<div class="text-xs font-bold bg-purple-500 px-6 pt-4 pb-12 ">
+  * 이 데이터는 채용 정보에 포함된 직무 별 요구 경력(최저,최대)과
+  연봉(최저,최대)을 바탕으로 추정한 예상 연봉 데이터입니다.
+</div>
+
+<div class="pb-10">
+  <div class="px-4  mt-4 flex justify-between font-bold  items-end bg-white">
+    서버 개발자
+  </div>
+
+  <div class="grid grid-cols-2 gap-4 pt-4 px-4 bg-white">
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">SRE/DevOps, MLOps Engineer</div>
+      <div class="mt-1 text-sm font-bold">123컴퍼니</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,000,000원</div>
+    </a>
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1594882645126-14020914d58d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE5fHxmaXRuZXNzfGVufDB8fHx8MTY0NDc0MTY3MA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">PHP개발자</div>
+      <div class="mt-1 text-sm font-bold">Cake Corp</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,200,000원</div>
+    </a>
+  </div>
+
+  <div class="grid grid-cols-2 gap-4 pt-16 px-4 bg-white">
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">서버개발자</div>
+      <div class="mt-1 text-sm font-bold">코믹랩(Comiclab)</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,000,000원</div>
+    </a>
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1518684079-3c830dcef090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">Back-end Developer(Node.js)</div>
+      <div class="mt-1 text-sm font-bold">fake Company</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,200,000원</div>
+    </a>
+  </div>
+</div>
+<div class="pb-10">
+  <div class="grid grid-cols-2 gap-4 pt-4 px-4 bg-white">
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">SRE/DevOps, MLOps Engineer</div>
+      <div class="mt-1 text-sm font-bold">123컴퍼니</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,000,000원</div>
+    </a>
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1594882645126-14020914d58d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE5fHxmaXRuZXNzfGVufDB8fHx8MTY0NDc0MTY3MA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">PHP개발자</div>
+      <div class="mt-1 text-sm font-bold">Cake Corp</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,200,000원</div>
+    </a>
+  </div>
+
+  <div class="grid grid-cols-2 gap-4 pt-16 px-4 bg-white">
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">서버개발자</div>
+      <div class="mt-1 text-sm font-bold">코믹랩(Comiclab)</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,000,000원</div>
+    </a>
+    <a href="/" class="grid grid-rows-2">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1518684079-3c830dcef090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="mt-2 font-bold">Back-end Developer(Node.js)</div>
+      <div class="mt-1 text-sm font-bold">fake Company</div>
+      <div class="text-gray-400 text-xs mt-2">서울 · 한국</div>
+      <div class="text-gray-700 text-xs mt-2">채용보상금 1,200,000원</div>
+    </a>
+  </div>
+</div>
