91a1a4b6690219beda9d6cf34857159db5790a03
canonn11 <myungkwanokk@gmail.com>
2022-07-07 18:39:19 +0900

    채용공고 상세페이지 구현

---
+++ src/routes/recruit/detail.svelte
@@ -0,0 +1,343 @@
+<div>
+  <img
+    class="w-full h-56 object-cover"
+    src="https://images.unsplash.com/photo-1600210492493-0946911123ea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDEyfHxpbnRlcmlvcnxlbnwwfHx8fDE2NDQ3NDM4Mjg&ixlib=rb-1.2.1&q=80&w=1080"
+  />
+</div>
+
+<div class="bg-gray-100">
+  <div class="p-4 bg-white">
+    <div class="text-lg ">풀스택 개발자(중급 이상)</div>
+    <div class="flex gap-4 py-2">
+      <span class=""> 123컴퍼니 </span>
+      <span class="text-gray-400"> 서울 · 한국 </span>
+    </div>
+    <div class="flex gap-4 p-2 text-sm">
+      <span class="bg-gray-100 rounded-full px-2 py-1">#재택근무</span>
+      <span class="bg-gray-100 rounded-full px-2 py-1">#판매,유통</span>
+    </div>
+  </div>
+  <div class="bg-white py-4">
+    <div class="bg-gray-100  mx-6 p-4 rounded-md relative">
+      <div class="font-black text-lg ">채용 보상금</div>
+      <div class="grid grid-cols-2">
+        <div class="mt-2">
+          <div class="text-gray-500">추천인</div>
+          <div class="font-black ">500,000원</div>
+        </div>
+        <div class="mt-2">
+          <div class="text-gray-500">지원자</div>
+          <div class="font-black">500,000원</div>
+        </div>
+      </div>
+      <div
+        class="absolute top-4 right-4 flex items-center justify-center border rounded-full w-10 h-10 border-blue-400 bg-white  text-blue-600"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-git-merge"
+        >
+          <circle cx="18" cy="18" r="3" />
+          <circle cx="6" cy="6" r="3" />
+          <path d="M6 21V9a9 9 0 0 0 9 9" />
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white">
+    <div class="flex justify-between bg-gray-100 p-3 mx-6 rounded-md">
+      <div class="flex  gap-2 text-xs">
+        <div class="font-bold">Finger AI</div>
+        <div class="">합격예측</div>
+      </div>
+      <div class="flex gap-1 text-gray-400 font-bold text-xs ">
+        서류합격률이 궁금하다면?
+        <a href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-help-circle"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+            <line x1="12" y1="17" x2="12.01" y2="17" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-white p-6">
+    <button
+      class="flex justify-center items-center border rounded-full w-10 h-10"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="gray"
+        stroke="gray"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-heart"
+        ><path
+          d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+        /></svg
+      >
+    </button>
+  </div>
+
+  <div class="bg-white px-4 text-sm text-gray-600 ">
+    <br /># WHO WE ARE<br />
+    "재미있고 건강한 패밀리 라이프스타일 주식회사, 핑거!"<br />
+    핑거는 끊임없는 소비자 연구와 제품 혁신을 통해 더욱 재미있고, 건강하고, 더욱
+    실용적인 제품을 개발하고, 합리적인 가격에 제공하는 가족 친화적인 기업입니다.<br
+    />
+    <br />
+    # 재택 근무<br />
+    <br />
+    ** 자신이 편안하다고 느끼는 곳, 자신의 퍼포먼스가 가장 높은 곳이라면 그곳이 바로
+    프로젝트 팀원이 일하는 곳입니다. 주 3회 재택 근무로 근무의 자율성을 존중합니다.**
+    <br />
+    <br />
+    # WHO WE ARE<br />
+    "재미있고 건강한 패밀리 라이프스타일 주식회사, 핑거!"<br />
+    핑거는 끊임없는 소비자 연구와 제품 혁신을 통해 더욱 재미있고, 건강하고, 더욱
+    실용적인 제품을 개발하고, 합리적인 가격에 제공하는 가족 친화적인 기업입니다.<br
+    />
+    <br />
+    # 재택 근무<br />
+    <br />
+    ** 자신이 편안하다고 느끼는 곳, 자신의 퍼포먼스가 가장 높은 곳이라면 그곳이 바로
+    프로젝트 팀원이 일하는 곳입니다. 주 3회 재택 근무로 근무의 자율성을 존중합니다.**
+    <br />
+    <br />
+    # WHO WE ARE<br />
+    "재미있고 건강한 패밀리 라이프스타일 주식회사, 핑거!"<br />
+    핑거는 끊임없는 소비자 연구와 제품 혁신을 통해 더욱 재미있고, 건강하고, 더욱
+    실용적인 제품을 개발하고, 합리적인 가격에 제공하는 가족 친화적인 기업입니다.<br
+    />
+    <br />
+    # 재택 근무<br />
+    <br />
+    ** 자신이 편안하다고 느끼는 곳, 자신의 퍼포먼스가 가장 높은 곳이라면 그곳이 바로
+    프로젝트 팀원이 일하는 곳입니다. 주 3회 재택 근무로 근무의 자율성을 존중합니다.**
+    <br />
+  </div>
+  <div class="bg-white">
+    <div class="p-4 font-black text-sm">기술스택 · 툴</div>
+    <div class="flex flex-wrap gap-2   border-b pb-6 mx-4">
+      <div
+        class="flex items-center whitespace-pre px-3  h-10 bg-green-200 rounded-full text-sm "
+      >
+        MySQL
+      </div>
+      <div
+        class="flex items-center  whitespace-pre px-3  h-10 rounded-full text-sm bg-green-200"
+      >
+        React
+      </div>
+      <div
+        class="flex items-center  whitespace-pre px-3  h-10 rounded-full text-sm bg-green-200"
+      >
+        JavaScript
+      </div>
+      <div
+        class="flex items-center  whitespace-pre px-3  h-10 rounded-full text-sm bg-green-200"
+      >
+        Node.js
+      </div>
+      <div
+        class="flex items-center  whitespace-pre px-3 h-10 rounded-full text-sm bg-green-200"
+      >
+        Typescript
+      </div>
+    </div>
+  </div>
+  <div class="bg-white pt-4">
+    <div class="flex px-4 py-1 text-sm">
+      <div class="text-gray-500 w-1/3">마감일</div>
+      <div class="font-black">상시</div>
+    </div>
+    <div class="flex px-4 py-1 text-sm">
+      <div class="text-gray-500 w-1/3">근무지역</div>
+      <div class="font-black">서울시 용산구 한강대로</div>
+    </div>
+    <img
+      class="w-full p-4  h-56 object-cover"
+      src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/89c35b49-7969-4e4f-a048-2cad8f58a94a.png"
+    />
+  </div>
+  <a href="/" class="bg-white  flex justify-between items-center p-4 mt-4">
+    <div class="flex gap-2">
+      <img
+        class="w-12 h-12"
+        src="https://images.unsplash.com/photo-1526913299589-f35a3ddeb7ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHxtb25leXxlbnwwfHx8fDE2NDQ3NDM5MjM&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="flex flex-col justify-center">
+        <div>123컴퍼니</div>
+        <div class="text-gray-400">판매,유통</div>
+      </div>
+    </div>
+    <button class="bg-blue-500 p-2 text-white rounded-sm text-sm">
+      팔로우
+    </button>
+  </a>
+  <div class="mt-4">
+    <div class="px-4 pt-4 flex justify-between font-bold  items-end bg-white">
+      이 포지션을 찾고 계셨나요?
+    </div>
+
+    <div class="grid grid-cols-2 gap-4 pt-4 px-4 bg-white">
+      <a href="/" class="grid grid-rows-2 relative">
+        <img
+          class="rounded-sm w-full h-28 object-cover"
+          src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+        />
+        <div class="mt-2 font-bold">백엔드(Node.js)개발자</div>
+        <div class="mt-1 text-sm font-bold">123컴퍼니</div>
+        <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+        <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+        <div class="absolute top-1 right-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="transparent"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bookmark"
+            ><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" /></svg
+          >
+        </div>
+      </a>
+      <a href="/" class="grid grid-rows-2 relative">
+        <img
+          class="rounded-sm w-full h-28 object-cover"
+          src="https://images.unsplash.com/photo-1594882645126-14020914d58d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE5fHxmaXRuZXNzfGVufDB8fHx8MTY0NDc0MTY3MA&ixlib=rb-1.2.1&q=80&w=1080"
+        />
+        <div class="mt-2 font-bold">Back-end 개발자(Spring,Python)</div>
+        <div class="mt-1 text-sm font-bold">Cake Corp</div>
+        <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+        <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+        <div class="absolute top-1 right-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="transparent"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bookmark"
+          >
+            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+          </svg>
+        </div>
+      </a>
+    </div>
+
+    <div class="grid grid-cols-2 gap-4 pt-12 px-4 bg-white">
+      <a href="/" class="grid grid-rows-2 relative">
+        <img
+          class="rounded-sm w-full h-28 object-cover"
+          src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+        />
+        <div class="mt-2 font-bold">서버개발자</div>
+        <div class="mt-1 text-sm font-bold">코믹랩(Comiclab)</div>
+        <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+        <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+        <div class="absolute top-1 right-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="transparent"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bookmark"
+          >
+            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+          </svg>
+        </div>
+      </a>
+      <a href="/" class="grid grid-rows-2 relative">
+        <img
+          class="rounded-sm w-full h-28 object-cover"
+          src="https://images.unsplash.com/photo-1518684079-3c830dcef090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+        />
+        <div class="mt-2 font-bold">Back-end Developer(Node.js)</div>
+        <div class="mt-1 text-sm font-bold">fake Company</div>
+        <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+        <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+        <div class="absolute top-1 right-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="transparent"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bookmark"
+          >
+            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+          </svg>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+
+<div class="sticky z-10 flex gap-4 bottom-0 left-0 right-0 px-4 p-4 bg-white ">
+  <button
+    href="/"
+    class="block text-center w-1/2 rounded-full font-black bg-white-500 rounded border border-blue-500 h-12 text-blue-500 flex items-center justify-center gap-1"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-bookmark"
+      ><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" /></svg
+    >북마크하기</button
+  >
+  <a
+    href="/"
+    class="block text-center w-1/2 rounded-full font-black bg-blue-500 rounded h-12 text-white flex items-center justify-center"
+    >지원하기</a
+  >
+</div>
