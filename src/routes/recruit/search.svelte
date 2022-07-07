7c9bde92cff5a41c5d90df05615fd4a2af5056a5
canonn11 <myungkwanokk@gmail.com>
2022-07-07 18:41:20 +0900

    채용 분야 검색페이지 구현

---
+++ src/routes/recruit/search.svelte
@@ -0,0 +1,167 @@
+<div
+  class="flex fixed z-10 w-full  top-0 h-14 items-center gap-4 px-4 bg-white"
+>
+  <a>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-arrow-left"
+    >
+      <line x1="19" y1="12" x2="5" y2="12" />
+      <polyline points="12 19 5 12 12 5" />
+    </svg>
+  </a>
+  <a
+    href="/search"
+    class="w-full border-none rounded  text-gray-400 placeholder-neutral-400"
+  >
+    회사, 포지션, 기술, 태그 검색
+  </a>
+</div>
+
+<div class="pt-12" />
+
+<div class="px-4 flex justify-between items-end pt-6 bg-white">
+  <div class=" font-black flex">관심 분야의 채용 공고를 탐색해보세요.</div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 mt-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">개발</div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDh8fGZhc2hpb258ZW58MHx8fHwxNjQ0NzQxNTkx&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">
+      경영·비즈니스
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1511556820780-d912e42b4980?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDMwfHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">
+      마케팅·광고
+    </div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1560343090-f0409e92791a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDEwfHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">디자인</div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">영업</div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1504593811423-6dd665756598?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE0fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">
+      고객서비스·리테일
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDR8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">HR</div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDJ8fHByb2R1Y3R8ZW58MHx8fHwxNjQ0NzQwNzIz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">게임 제작</div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1564466809058-bf4114d55352?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">미디어</div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1554469384-e58fac16e23a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDN8fGJ1aWxkaW5nfGVufDB8fHx8MTY0NDc0Mzg0NA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">
+      엔지니어링·설계
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 px-4 py-2 bg-white">
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1593672715438-d88a70629abe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDN8fG1vbmV5fGVufDB8fHx8MTY0NDc0MzkyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">금융</div>
+  </div>
+  <div class="relative">
+    <a href="/" class="">
+      <img
+        class="rounded-sm w-full h-28 object-cover"
+        src="https://images.unsplash.com/photo-1528702748617-c64d49f918af?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDMwfHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </a>
+    <div class="absolute font-black text-white bottom-2 left-2">물류·무역</div>
+  </div>
+</div>
