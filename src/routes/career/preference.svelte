e1d32adfd678a14d90dabc292ba3a04c7ded94fa
canonn11 <myungkwanokk@gmail.com>
2022-07-07 18:37:58 +0900

    직무 설정 페이지 구현

---
+++ src/routes/career/preference.svelte
@@ -0,0 +1,200 @@
+<div class="sticky top-0 z-10 bg-white">
+  <div class="flex justify-between border-b p-3">
+    <button class="flex pl-2 items-center gap-2 w-1/3 text-gray-400 text-sm">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-rotate-ccw"
+      >
+        <polyline points="1 4 1 10 7 10" />
+        <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10" />
+      </svg>초기화
+    </button>
+    <div
+      class="flex gap-1 w-1/3 justify-center items-center font-black  text-center bg-white"
+    >
+      직무
+      <div
+        class="flex items-center justify-center text-sm  rounded-full border w-6 h-6 bg-blue-600 text-white"
+      >
+        1
+      </div>
+    </div>
+    <a href="/" class="text-gray-500 w-1/3 flex justify-end"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-x"
+      >
+        <line x1="18" y1="6" x2="6" y2="18" />
+        <line x1="6" y1="6" x2="18" y2="18" />
+      </svg></a
+    >
+  </div>
+</div>
+
+<div class="text-sm text-gray-400 p-4">
+  직무를 선택해 주세요. (최대 5개 선택 가능)
+</div>
+
+<div class="flex flex-wrap gap-3 px-4">
+  <button
+    class="whitespace-pre px-3 my-1 h-10 border-2 border-blue-600 text-blue-600 font-bold rounded-full text-sm "
+  >
+    개발 전체
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    웹 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    서버 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    프론트엔드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    소프트웨어 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    자바 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    안드로이드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    iOS 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    데이터 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    Node.js 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    파이썬 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    프론트엔드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    소프트웨어 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    자바 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    안드로이드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    iOS 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    데이터 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    Node.js 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    파이썬 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    프론트엔드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    소프트웨어 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    자바 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    안드로이드 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    iOS 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    데이터 엔지니어
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    Node.js 개발자
+  </button>
+  <button
+    class="whitespace-pre px-3 my-1 h-10 rounded-full text-sm bg-gray-200"
+  >
+    파이썬 개발자
+  </button>
+</div>
+
+<div
+  class="sticky bottom-0 z-10 left-0 right-0 px-4 p-4 border-t border-gray-300 bg-white "
+>
+  <a
+    href="/"
+    class="block text-center w-full rounded-full font-black bg-blue-500 rounded h-12 text-sm text-white flex items-center justify-center"
+    >선택 완료하기</a
+  >
+</div>
