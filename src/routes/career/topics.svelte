1fb5b7594a0045760ab69d139384947a78754ebc
canonn11 <myungkwanokk@gmail.com>
2022-07-10 21:27:23 +0900

    추천 토픽 페이지 구현

---
+++ src/routes/career/topics.svelte
@@ -0,0 +1,221 @@
+<div class="fixed w-full z-10 top-0 bg-white flex justify-between border-b">
+  <div class="flex justify-center items-center gap-4 p-4">
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12" />
+        <polyline points="12 19 5 12 12 5" />
+      </svg></a
+    >
+    <div class=" font-black text-center bg-white">추천 토픽</div>
+  </div>
+</div>
+<div class="p-8" />
+<div class="flex justify-end items-center p-4 gap-4 mr-4">
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="gray"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-server"
+    >
+      <rect x="2" y="2" width="20" height="8" rx="2" ry="2" />
+      <rect x="2" y="14" width="20" height="8" rx="2" ry="2" />
+      <line x1="6" y1="6" x2="6.01" y2="6" />
+      <line x1="6" y1="18" x2="6.01" y2="18" />
+    </svg>
+  </button>
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-list"
+    >
+      <line x1="8" y1="6" x2="21" y2="6" />
+      <line x1="8" y1="12" x2="21" y2="12" />
+      <line x1="8" y1="18" x2="21" y2="18" />
+      <line x1="3" y1="6" x2="3.01" y2="6" />
+      <line x1="3" y1="12" x2="3.01" y2="12" />
+      <line x1="3" y1="18" x2="3.01" y2="18" />
+    </svg>
+  </button>
+</div>
+
+<div class="px-4 space-y-4">
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover"
+        src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE2fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 너 뭐 돼? 그래 나 뭐 돼!</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        초보 프리랜서를 위한 프로젝트 선택 가이드
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDh8fGJ1aWxkaW5nfGVufDB8fHx8MTY0NDc0Mzg0NA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">이 회사 어때요?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        팬데믹 속에서 오히려 성장하는 회사
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1638913660106-73b4bac0db09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDIyfHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 망설여진다면?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        지금 프리한 혜택을 살펴보세요!
+      </div>
+    </div>
+  </a>
+</div>
+
+<div class="p-4">
+  <img
+    class="w-full rounded-md border-2"
+    src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/2ed7068d-1a96-49ea-9e71-d2a6a00a594e.PNG"
+  />
+</div>
+
+<div class="px-4 space-y-4">
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover"
+        src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE2fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 너 뭐 돼? 그래 나 뭐 돼!</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        초보 프리랜서를 위한 프로젝트 선택 가이드
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDh8fGJ1aWxkaW5nfGVufDB8fHx8MTY0NDc0Mzg0NA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">이 회사 어때요?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        팬데믹 속에서 오히려 성장하는 회사
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1638913660106-73b4bac0db09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDIyfHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 망설여진다면?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        지금 프리한 혜택을 살펴보세요!
+      </div>
+    </div>
+  </a>
+</div>
+
+<div class="p-4">
+  <img
+    class="w-full rounded-md border-2"
+    src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/2ed7068d-1a96-49ea-9e71-d2a6a00a594e.PNG"
+  />
+</div>
+
+<div class="px-4 space-y-4">
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover"
+        src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE2fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 너 뭐 돼? 그래 나 뭐 돼!</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        초보 프리랜서를 위한 프로젝트 선택 가이드
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDh8fGJ1aWxkaW5nfGVufDB8fHx8MTY0NDc0Mzg0NA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">이 회사 어때요?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        팬데믹 속에서 오히려 성장하는 회사
+      </div>
+    </div>
+  </a>
+  <a href="/" class="flex w-full gap-4">
+    <div class="flex items-end overflow-hidden  border rounded bg-white">
+      <img
+        class="w-24 h-24 object-cover "
+        src="https://images.unsplash.com/photo-1638913660106-73b4bac0db09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDIyfHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div class="w-52 space-y-2">
+      <div class="font-bold">프리랜서, 망설여진다면?</div>
+      <div class="w-52 text-sm text-gray-400 truncate">
+        지금 프리한 혜택을 살펴보세요!
+      </div>
+    </div>
+  </a>
+</div>
+
+<div class="p-4">
+  <img
+    class="w-full rounded-md border-2"
+    src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/2ed7068d-1a96-49ea-9e71-d2a6a00a594e.PNG"
+  />
+</div>
