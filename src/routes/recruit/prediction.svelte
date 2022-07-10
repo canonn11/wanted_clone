25b92a9f0ad04aca928091f63af052bd848fd367
canonn11 <myungkwanokk@gmail.com>
2022-07-10 21:29:11 +0900

    합격예측 페이지 구현

---
+++ src/routes/recruit/prediction.svelte
@@ -0,0 +1,263 @@
+<div class="sticky z-10 w-full top-0 bg-white flex  border-b">
+  <div class="flex justify-center items-center gap-2 p-4">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-arrow-left"
+    >
+      <line x1="19" y1="12" x2="5" y2="12" />
+      <polyline points="12 19 5 12 12 5" />
+    </svg>
+    <div class=" font-black text-center ">Fingr AI</div>
+    <div class=" text-center">합격예측</div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-help-circle"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <line x1="12" y1="17" x2="12.01" y2="17" />
+    </svg>
+  </div>
+</div>
+
+<div class="px-4 pt-8">
+  <div class=" font-black mt-2">
+    핑거에서 나에게 딱 맞는 포지션을<br />추천해 드립니다.
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-4 pt-8 pb-4 px-4 bg-white">
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">백엔드(Node.js)개발자</div>
+    <div class="mt-1 text-xs font-bold">123컴퍼니</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1594882645126-14020914d58d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE5fHxmaXRuZXNzfGVufDB8fHx8MTY0NDc0MTY3MA&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">Back-end 개발자(Spring,Python)</div>
+    <div class="mt-1 text-sm font-bold">Cake Corp</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+</div>
+
+<div class="grid grid-cols-2 gap-4 pt-12 px-4 bg-white">
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">서버개발자</div>
+    <div class="mt-1 text-sm font-bold">코믹랩(Comiclab)</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1518684079-3c830dcef090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">Back-end Developer(Node.js)</div>
+    <div class="mt-1 text-sm font-bold">fake Company</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+</div>
+
+<div class="grid grid-cols-2 gap-4 py-8 px-4 bg-white">
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDE1fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">백엔드(Node.js)개발자</div>
+    <div class="mt-1 text-sm font-bold">123컴퍼니</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1594882645126-14020914d58d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE5fHxmaXRuZXNzfGVufDB8fHx8MTY0NDc0MTY3MA&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">Back-end 개발자(Spring,Python)</div>
+    <div class="mt-1 text-sm font-bold">Cake Corp</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+</div>
+
+<div class="grid grid-cols-2 gap-4 pt-12 px-4 bg-white">
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1638913662295-9630035ef770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwxfDF8c2VhcmNofDI5fHxwcm9kdWN0fGVufDB8fHx8MTY0NDc0MDcyMw&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">서버개발자</div>
+    <div class="mt-1 text-sm font-bold">코믹랩(Comiclab)</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,000,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+  <a href="/" class="grid grid-rows-2 relative">
+    <img
+      class="rounded-md w-full h-28 object-cover"
+      src="https://images.unsplash.com/photo-1518684079-3c830dcef090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI0fHx0cmF2ZWx8ZW58MHx8fHwxNjQ0NzQxNjgy&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="mt-2 font-bold">Back-end Developer(Node.js)</div>
+    <div class="mt-1 text-sm font-bold">fake Company</div>
+    <div class="text-gray-400 text-xs mt-1">서울 · 한국</div>
+    <div class="text-gray-700 text-xs mt-1">채용보상금 1,200,000원</div>
+    <div class="absolute top-1 right-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="transparent"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+  </a>
+</div>
