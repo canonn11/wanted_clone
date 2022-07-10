d3a5d9c47cf9e58cf61293200fbe21cd8b4b9f0c
canonn11 <myungkwanokk@gmail.com>
2022-07-10 21:30:55 +0900

    커뮤니티 세부글 페이지 구현

---
+++ src/routes/community/articledetail.svelte
@@ -0,0 +1,241 @@
+<div
+  class=" fixed top-0 z-10 w-full flex justify-between text-xl p-4 flex items-center border-b border-gray-300  bg-white"
+>
+  <a href="/">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-left"
+    >
+      <polyline points="15 18 9 12 15 6" />
+    </svg>
+  </a>
+  <a href="/">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-more-horizontal"
+    >
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="19" cy="12" r="1" />
+      <circle cx="5" cy="12" r="1" />
+    </svg>
+  </a>
+</div>
+
+<div class="px-4 pt-16 pb-4">
+  <div class=" font-black mt-2">
+    기술트렌드 파악에 도움되는 최신기술이슈 모음집 공유해요
+  </div>
+</div>
+
+<div class="mx-4 pb-6 pt-2 flex items-center gap-2 ">
+  <img
+    class="w-8 h-8 object-cover rounded-full"
+    src="https://images.unsplash.com/photo-1580489944761-15a19d654956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE4fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+  />
+  <div class="text-sm font-bold ">비빔밥</div>
+  <div class="text-xs font-bold text-gray-400">·</div>
+  <div class="text-xs text-gray-400 ">2022.06.29</div>
+</div>
+
+<div class="border-t mx-4 py-6 ">
+  안녕하세요!<br /> 매일 알아보는 기술이슈 5와 특정 주제에 관한 기술TMI를 함께
+  공유드려요~<br />
+  가볍게 살펴보시고 도움되신다면 좋아요 혹은 댓글 부탁드려요.<br />
+  관심있는 기술 분야가 있으시면 알려주세요! 다음 기술 TMI에 참고해보겠습니다.<br
+  />
+  좋은 하루 보내세요<br />
+  <br />
+  안녕하세요!<br /> 매일 알아보는 기술이슈 5와 특정 주제에 관한 기술TMI를 함께
+  공유드려요~<br />
+  가볍게 살펴보시고 도움되신다면 좋아요 혹은 댓글 부탁드려요.<br />
+  관심있는 기술 분야가 있으시면 알려주세요! 다음 기술 TMI에 참고해보겠습니다.<br
+  />
+  좋은 하루 보내세요<br />
+  <br />
+  안녕하세요!<br /> 매일 알아보는 기술이슈 5와 특정 주제에 관한 기술TMI를 함께
+  공유드려요~<br />
+  가볍게 살펴보시고 도움되신다면 좋아요 혹은 댓글 부탁드려요.<br />
+  관심있는 기술 분야가 있으시면 알려주세요! 다음 기술 TMI에 참고해보겠습니다.<br
+  />
+  좋은 하루 보내세요<br />
+</div>
+
+<div class="flex items-center gap-2 px-4 py-12 ">
+  <div
+    class="text-xs p-1 text-center text-gray-400 border rounded-full whitespace-pre "
+  >
+    개발
+  </div>
+  <div
+    class="text-xs p-1 text-center text-gray-400 border rounded-full whitespace-pre"
+  >
+    취업/이직
+  </div>
+  <div
+    class="text-xs p-1 text-center text-gray-400 border rounded-full whitespace-pre"
+  >
+    IT/기술
+  </div>
+  <div
+    class="text-xs p-1 text-center text-gray-400 border rounded-full whitespace-pre"
+  >
+    서비스기획
+  </div>
+</div>
+
+<div class="flex gap-2 px-4 pb-6 pt-4  border-b ">
+  <button
+    href="/"
+    class="block text-center w-1/2 rounded-lg font-black bg-gray-100 text-sm h-10 text-gray-500 flex items-center justify-center gap-1"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-thumbs-up"
+      ><path
+        d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"
+      /></svg
+    ><span>좋아요</span> <span>4</span></button
+  >
+  <button
+    href="/"
+    class="block text-center w-1/2 rounded-lg font-black bg-gray-100 text-sm  h-10 text-gray-500 flex items-center justify-center gap-1"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="gray"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-message-circle"
+    >
+      <path
+        d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+      />
+    </svg><span>댓글</span> <span>2</span></button
+  >
+</div>
+
+<div class="bg-white flex flex-col ">
+  <div class="mx-4  pt-6 pb-4 flex justify-between gap-2 pr-2 ">
+    <div class="flex items-center justify-between gap-2">
+      <img
+        class="w-6 h-6 object-cover rounded-full"
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE4fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-sm font-bold ">케이</div>
+      <div class="text-xs font-bold text-gray-400">·</div>
+      <div class="text-xs text-gray-400 ">2022.06.09</div>
+    </div>
+    <button class="text-gray-500">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-horizontal"
+      >
+        <circle cx="12" cy="12" r="1" />
+        <circle cx="19" cy="12" r="1" />
+        <circle cx="5" cy="12" r="1" />
+      </svg>
+    </button>
+  </div>
+  <div class="pl-12 pr-8 pb-8">
+    <div class=" text-sm ">기술 분야에 대한 뉴스레터인가요?</div>
+  </div>
+</div>
+
+<div class="bg-white flex flex-col ">
+  <div class="mx-4 border-t pt-6 pb-4 flex justify-between gap-2 pr-2 ">
+    <div class="flex items-center justify-between gap-2">
+      <img
+        class="w-6 h-6 object-cover rounded-full"
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE4fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-sm font-bold ">비빔밥</div>
+      <div class="text-xs font-bold text-gray-400">·</div>
+      <div class="text-xs text-gray-400 ">2022.06.09</div>
+    </div>
+    <button class="text-gray-500">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-horizontal"
+      >
+        <circle cx="12" cy="12" r="1" />
+        <circle cx="19" cy="12" r="1" />
+        <circle cx="5" cy="12" r="1" />
+      </svg>
+    </button>
+  </div>
+  <div class="pl-12 pr-8 pb-8">
+    <div class=" text-sm ">
+      넵 기술 전반의 최신 소식을 다루는 뉴스레터를 매일 발행하고 있어요~
+      도움드릴수 있는 콘텐츠를 만든거고 사이트에서는 더 많은 소식을 한번에 볼 수
+      있어요.
+    </div>
+  </div>
+</div>
+
+<div class="pt-14" />
+
+<div
+  class="fixed bottom-0 bg-white z-10 flex items-center gap-2 p-1 w-full h-14  border-t  text-gray-700"
+>
+  <div class="p-2">
+    <img
+      class="w-6 h-6 object-cover rounded-full"
+      src="https://images.unsplash.com/photo-1580489944761-15a19d654956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE4fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+  </div>
+  <div
+    class="flex justify-between items-center bg-gray-100  border mr-2  pr-2 w-full rounded-lg"
+  >
+    <input
+      type="text"
+      class="w-64 border-none rounded bg-gray-100 placeholder-gray-400"
+      placeholder="댓글 남기기"
+    />
+    <div class="text-gray-300 text-sm">등록</div>
+  </div>
+</div>
