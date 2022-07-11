66c446694201fbb784f283729f4a250de20f8e96
canonn11 <myungkwanokk@gmail.com>
2022-07-11 23:27:55 +0900

    글 작성 페이지 구현

---
+++ src/routes/community/post.svelte
@@ -0,0 +1,149 @@
+<div class="bg-white h-14 px-4 items-center border-b flex">
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-x"
+      ><line x1="18" y1="6" x2="6" y2="18" /><line
+        x1="6"
+        y1="6"
+        x2="18"
+        y2="18"
+      /></svg
+    >
+  </button>
+  <div
+    class="flex flex-1 justify-center pr-8 font-bold text-base text-gray-800"
+  >
+    글 작성하기
+  </div>
+</div>
+
+<div class="flex gap-1 px-4 pt-4 ">
+  <div class="font-bold ">태그 선택</div>
+  <div class="text-gray-400">(1~3개)</div>
+  <div class="text-red-600">*</div>
+</div>
+
+<div class="flex gap-2 mt-3 px-4">
+  <button
+    class="bg-gray-200 rounded-full flex justify-center items-center w-8 h-8"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="gray"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-plus"
+      ><line x1="12" y1="5" x2="12" y2="19" /><line
+        x1="5"
+        y1="12"
+        x2="19"
+        y2="12"
+      /></svg
+    >
+  </button>
+  <button
+    class="flex justify-center items-center gap-1  px-2 rounded-full border border-blue-600 bg-white py-1"
+  >
+    <div class="text-blue-600 font-bold text-sm">커리어고민</div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="blue"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-x"
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  </button>
+  <button
+    class="flex justify-center items-center gap-1  px-2 rounded-full border border-blue-600 bg-white py-1"
+  >
+    <div class="text-blue-600 font-bold text-sm">회사생활</div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="blue"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-x"
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  </button>
+</div>
+
+<div class="px-2 pt-4">
+  <input
+    type="text"
+    class="w-full h-12  rounded border-none  font-black placeholder-neutral-400"
+    placeholder="제목을 입력해주세요."
+  />
+</div>
+<div class="pt-1 border-t mx-4" />
+<div class="px-2 pt-1">
+  <textarea
+    class="flex w-full h-96 border-none border-gray-300 rounded  placeholder-gray-400"
+    placeholder="내용을 작성해주세요."
+  />
+</div>
+
+<div class=" mt-8 left-0 right-0 px-4 p-4 border-gray-300 bg-white ">
+  <button
+    href="/"
+    class="text-center w-full rounded-full font-black bg-gray-200 rounded h-12 text-gray-500 flex items-center gap-1 justify-center"
+  >
+    등록하기</button
+  >
+</div>
+<div class="border-t px-2 h-14 ">
+  <button class="grid grid-rows-2 h-10 w-10 justify-center items-center p-1 ">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-image"
+    >
+      <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+      <circle cx="8.5" cy="8.5" r="1.5" />
+      <polyline points="21 15 16 10 5 21" />
+    </svg>
+    <div class="flex text-xs font-bold">
+      <div>(</div>
+      <div class="text-blue-600">0</div>
+      <div>/</div>
+      <div>1</div>
+      <div>)</div>
+    </div>
+  </button>
+</div>
