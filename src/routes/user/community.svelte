7dbeead5c86c8270b75a3d85af7613ab74e35c17
canonn11 <myungkwanokk@gmail.com>
2022-07-11 23:29:10 +0900

    MY커뮤니티 페이지 구현

---
+++ src/routes/user/community.svelte
@@ -0,0 +1,181 @@
+<div class="bg-white h-14 px-4 items-center border-b flex">
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-left"
+      ><polyline points="15 18 9 12 15 6" /></svg
+    >
+  </button>
+  <div
+    class="flex flex-1 justify-center pr-8 font-bold text-base text-gray-800"
+  >
+    MY 커뮤니티
+  </div>
+</div>
+
+<div class="flex justify-between items-center p-4">
+  <div class="flex gap-2 items-center">
+    <img
+      class="w-10 h-10 rounded-full object-cover"
+      src="https://images.unsplash.com/photo-1554151228-14d9def656e4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDZ8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+    />
+    <div class="font-bold text-lg ">홍길동</div>
+    <div class="text-xs font-bold text-blue-600">기본</div>
+  </div>
+  <div class="">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-edit-2"
+    >
+      <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+    </svg>
+  </div>
+</div>
+
+<div class="bg-gray-100">
+  <div class="pt-3" />
+  <div class="bg-white">
+    <div class="flex gap-1 px-4 pt-6 pb-2 ">
+      <div class="font-bold ">내 활동</div>
+    </div>
+    <div class="flex items-center border-b gap-2 px-4 mt-4">
+      <button class=" border-b border-blue-500 pb-1 text-blue-500 font-black "
+        >최근본</button
+      >
+      <button class=" text-gray-400 font-black pb-1 mb-0.5 ">작성글</button>
+      <button class=" text-gray-400 font-black pb-1 mb-0.5 ">작성댓글</button>
+      <button class=" text-gray-400 font-black pb-1 mb-0.5">좋아요</button>
+    </div>
+  </div>
+  <div class="bg-white flex flex-col px-4 pb-4 ">
+    <div class=" space-y-2">
+      <div class=" font-black truncate  pt-4">
+        기술트렌드 파악에 도움되는 최신기술이슈 모음집
+      </div>
+      <div class="truncate text-sm ">
+        안녕하세요!<br />매일 알아보는 기술이슈 5와 특정 주제에 관한 기술을
+      </div>
+
+      <div class="flex text-xs items-center gap-1  text-gray-500">
+        <div class="mr-1">비빔밥</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="gray"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-thumbs-up"
+        >
+          <path
+            d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"
+          />
+          <span class="mr-2 font-black">2</span>
+        </svg><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="gray"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-message-circle"
+        >
+          <path
+            d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+          />
+        </svg>
+        <span class="font-black">4</span>
+        <span class="font-black">·</span>
+        <span>2022.06.29</span>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white flex flex-col px-4 pb-4 ">
+    <div class=" space-y-2">
+      <div class=" font-black truncate border-t pt-4">
+        신입이 경력직 공고 지원할 때 보통 어떤 걸 제출
+      </div>
+      <div class="truncate text-sm ">
+        얼마 전 인턴 마치고 슬슬 취준 중입니다. 주변에서<br />는 신입 공고나
+        경력직 공고 가리지 말고 무조건 다
+      </div>
+
+      <div class="flex text-xs items-center gap-1  text-gray-500">
+        <div class="mr-1">비빔밥</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="gray"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-thumbs-up"
+        >
+          <path
+            d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"
+          />
+          <span class="mr-2 font-black">2</span>
+        </svg>
+        <span class="font-black">·</span>
+        <span>2022.06.30</span>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white flex flex-col px-4 pb-4 ">
+    <div class=" space-y-2">
+      <div class=" font-black truncate border-t pt-4">서류열람</div>
+      <div class="truncate text-sm ">
+        혹시 서류 열람만 하고 결과를 안 알려주기도 하나<br />요..? 하나는 17일날
+        열람하셨고 하나는 29일에
+      </div>
+
+      <div class="flex text-xs items-center gap-1  text-gray-500">
+        <div class="mr-1">비빔밥</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="gray"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-message-circle"
+        >
+          <path
+            d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+          />
+        </svg>
+        <span class="font-black">1</span>
+        <span class="font-black">·</span>
+        <span>14시간 전</span>
+      </div>
+    </div>
+  </div>
+</div>
