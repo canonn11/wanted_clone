026ded10f382ba5c293470e68a4deac7efc93adb
canonn11 <myungkwanokk@gmail.com>
2022-07-11 23:30:05 +0900

    커뮤니티 프로필 페이지 구현

---
+++ src/routes/user/communityprofile.svelte
@@ -0,0 +1,79 @@
+<div class="bg-white h-14 px-4 items-center border-b flex">
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-left"
+      ><polyline points="15 18 9 12 15 6" /></svg
+    >
+  </button>
+  <div
+    class="flex flex-1 justify-center pr-8 font-bold text-base text-gray-800"
+  >
+    커뮤니티 프로필
+  </div>
+</div>
+
+<div class="flex justify-between border-b my-2">
+  <div class="flex items-center p-4">
+    <input
+      class="h-5 w-5 mr-2 border border-gray-400 rounded-full bg-white checked:bg-gray-800 checked:border-gray-800 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+      type="checkbox"
+      value=""
+      id="all"
+    />
+    <div class="inline-block  text-gray-700" for="all">닉네임</div>
+  </div>
+  <div class="flex gap-2 items-center px-2 py-4 justify-center">
+    <div class="text-blue-500 font-bold">설정하기</div>
+    <div>
+      <img
+        class="w-7 h-7 rounded-full object-cover"
+        src="https://images.unsplash.com/photo-1554151228-14d9def656e4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDZ8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="gray"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-right"
+        ><polyline points="9 18 15 12 9 6" /></svg
+      >
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-between border-b my-2">
+  <div class="flex items-center p-4">
+    <input
+      class="h-5 w-5 mr-2 border border-gray-400 rounded-full bg-white checked:bg-gray-800 checked:border-gray-800 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+      type="checkbox"
+      value=""
+      id="all"
+    />
+    <div class="inline-block  text-gray-700" for="all">기본</div>
+  </div>
+  <div class="flex gap-2 items-center p-4 justify-center">
+    <div class=" font-bold">홍길동</div>
+    <div>
+      <img
+        class="w-7 h-7 rounded-full object-cover"
+        src="https://images.unsplash.com/photo-1554151228-14d9def656e4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDZ8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    </div>
+  </div>
+</div>
