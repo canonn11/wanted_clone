a1c29321fce97cd8f5fe1504915b05f9b1ef675e
canonn11 <myungkwanokk@gmail.com>
2022-07-12 22:45:17 +0900

    프로필 페이지 구현

---
+++ src/routes/user/profile.svelte
@@ -0,0 +1,518 @@
+<div class="flex justify-between border-b ">
+  <div class=" p-4">
+    <div class="text-lg  text-center">MY 핑거</div>
+  </div>
+
+  <div class="flex items-center justify-center">
+    <button class="flex mx-3 gap-1">
+      <div class="flex">
+        <img
+          class="w-7 g-6 object-fit"
+          src="https://whvntffyxprmvalmnwnb.supabase.co/storage/v1/object/public/app/2242e6bd-505a-47d8-851f-9e165004b0e6.png"
+        />
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-down"
+        ><polyline points="6 9 12 15 18 9" /></svg
+      >
+    </button>
+    <button class=" text-sm p-3"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-vertical"
+      >
+        <circle cx="12" cy="12" r="1" />
+        <circle cx="12" cy="5" r="1" />
+        <circle cx="12" cy="19" r="1" />
+      </svg></button
+    >
+  </div>
+</div>
+
+<div class="flex justify-between  p-4">
+  <div class="flex items-center gap-4 ">
+    <div class="relative flex justify-center w-12 h-12">
+      <img
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDE4fHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+        class="w-12 h-12 border rounded-full object-cover"
+      />
+      <div
+        class="absolute left-7 top-6 flex items-center justify-center w-7 h-7 border rounded-full bg-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="gray"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-camera"
+        >
+          <path
+            d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
+          />
+          <circle cx="12" cy="13" r="4" />
+        </svg>
+      </div>
+    </div>
+
+    <div class="font-bold text-lg  ">홍길동</div>
+  </div>
+
+  <div
+    class="flex items-center justify-center font-bold text-lg border rounded-full w-36 "
+  >
+    1,000P
+  </div>
+</div>
+
+<div class="bg-white mt-4">
+  <div
+    class="flex justify-between gap-2  items-center bg-gray-100 p-3 mx-6 rounded-full"
+  >
+    <div class="flex gap-2 items-center">
+      <button
+        ><div
+          class="w-5 h-5 flex justify-center ml-1 pb-0.5 items-center bg-blue-600 text-white  rounded-md font-bold border-black text-xs "
+        >
+          N
+        </div></button
+      >
+      <div class="flex  gap-2 text-xs">
+        <div class="font-bold">요즘 내 관심사는? 맞춤 콘텐츠 받기!</div>
+      </div>
+    </div>
+    <div class="">
+      <a href="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="gray"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-chevron-right"
+        >
+          <polyline points="9 18 15 12 9 6" />
+        </svg>
+      </a>
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-3 px-4 py-2 border-b">
+  <button class="w-full space-y-2 py-2"
+    ><div class="flex justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-heart"
+      >
+        <path
+          d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+        />
+      </svg>
+    </div>
+    <div class="flex gap-1 justify-center">
+      <div class="">좋아요</div>
+      <div class="font-bold ">0</div>
+    </div>
+  </button>
+
+  <button class="w-full space-y-2 py-2"
+    ><div class="flex justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bookmark"
+      >
+        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+      </svg>
+    </div>
+    <div class="flex gap-1 justify-center">
+      <div class="">북마크</div>
+      <div class="font-bold ">0</div>
+    </div>
+  </button>
+  <button class="w-full space-y-2 py-2"
+    ><div class="flex justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-clipboard"
+      >
+        <path
+          d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
+        />
+        <rect x="8" y="2" width="8" height="4" rx="1" ry="1" />
+      </svg>
+    </div>
+    <div class="flex gap-1 justify-center">
+      <div class="">관심회사</div>
+      <div class="font-bold ">0</div>
+    </div>
+  </button>
+</div>
+
+<div class="bg-gray-100">
+  <div class="pt-2" />
+  <div class="bg-white p-4">
+    <div class="flex h-12 justify-between items-center  ">
+      <div class="mt-2 flex  gap-1 font-bold text-lg flex ">
+        프로필
+        <button
+          ><div
+            class="w-5 h-5 flex justify-center ml-1 pb-0.5  items-center bg-blue-600 text-white  rounded-md font-bold border-black text-xs "
+          >
+            N
+          </div></button
+        >
+      </div>
+
+      <select class="flex text-center h-9 pr-7 text-sm w-28 border rounded-lg">
+        <option selected>관심 있음</option>
+      </select>
+    </div>
+
+    <div class="p-4 bg-gray-100 rounded-md mt-4 space-y-1">
+      <div class="flex gap-2 justify-center items-center ">
+        <div
+          class="font-bold border px-2 rounded-lg text-gray-500 border-gray-400 "
+        >
+          프로
+        </div>
+        <div class="font-bold">신입</div>
+      </div>
+      <div class="text-center font-bold  ">신입, 웹 개발자</div>
+
+      <div class="flex-col">
+        <div class="relative">
+          <div class="flex  justify-center items-center  gap-0.5 w-full h-10">
+            <div class="w-10 h-2 bg-blue-500" />
+            <div class="w-10 h-2 bg-blue-500" />
+            <div class="w-10 h-2 bg-blue-500" />
+            <div class="w-10 h-2 bg-blue-500" />
+            <div class="w-10 h-2 bg-blue-500" />
+            <div class="w-10 h-2 bg-gray-200" />
+            <div class="w-10 h-2 bg-gray-200" />
+          </div>
+          <div class="absolute bg-blue-500 h-2 w-2 rounded-full top-4" />
+          <div
+            class="absolute bg-gray-200 h-2 w-2 rounded-full top-4 right-0"
+          />
+
+          <div
+            class="absolute h-6 w-6 rounded-full border-2 border-blue-500 flex justify-center items-center bg-white top-2 left-48 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="blue"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-lock"
+            >
+              <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
+              <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+            </svg>
+          </div>
+          <div
+            class="absolute h-6 w-6 rounded-full border-2 border-gray-200 flex justify-center items-center bg-white top-2 right-0 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              viewBox="0 0 24 24"
+              fill="gray"
+              stroke="gray"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-star"
+            >
+              <polygon
+                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div
+          class="flex gap-12 whitespace-pre text-gray-500 text-xs font-bold ml-10 "
+        >
+          <div class="">뉴비</div>
+          <div class="">루키</div>
+          <div class="ml-2">프로</div>
+          <div class="">마스터</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="text-sm px-4 bg-white text-center  ">
+    서류 합격률 높은 포지션을 알려드려요!
+  </div>
+
+  <div class=" left-0 right-0 px-8 bg-white py-4 ">
+    <a
+      href="/"
+      class="text-center w-full rounded-full font-black border border-blue-600 rounded h-12 text-sm text-blue-600 flex items-center gap-1 justify-center"
+    >
+      매치업 이력서 채우고 포지션 추천받기</a
+    >
+  </div>
+
+  <div class="bg-white mb-2 pb-4 border-b">
+    <div class="py-4 mx-4 mt-2  text-lg font-black">제안받기 현황</div>
+
+    <div class="grid grid-cols-3 bg-gray-100 mx-4 rounded-md px-2 py-4  ">
+      <div class=" w-full text-center space-y-2">
+        <div class="text-lg">0</div>
+        <div class="text-sm">관심 있음</div>
+      </div>
+      <div class=" w-full text-center space-y-2">
+        <div class="text-lg">0</div>
+        <div class="text-sm">열람</div>
+      </div>
+      <div class=" w-full text-center space-y-2">
+        <div class="text-lg">0</div>
+        <div class="text-sm">받은 제안</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-white pb-6 border-b">
+    <div class="py-4 mx-4 my-2  text-lg font-black">지원 현황</div>
+
+    <div class="grid grid-cols-4 px-3 rounded-md  ">
+      <div class=" w-full text-center space-y-2 border-r border-gray-300">
+        <div class="text-lg">0</div>
+        <div class="text-sm">지원 완료</div>
+      </div>
+      <div class=" w-full text-center space-y-2 border-r border-gray-300">
+        <div class="text-lg">0</div>
+        <div class="text-sm">진행 중</div>
+      </div>
+      <div class=" w-full text-center space-y-2 border-r border-gray-300">
+        <div class="text-lg">0</div>
+        <div class="text-sm">최종 합격</div>
+      </div>
+      <div class=" w-full text-center space-y-2">
+        <div class="text-lg">0</div>
+        <div class="text-sm">불합격</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-white  ">
+    <div class="border-b ">
+      <div class="py-4 mx-4 mt-2 mb-2  text-lg font-black">MY 핑거플러스</div>
+      <div class="px-4 pb-4 text-sm text-center text-gray-500 ">
+        직군별 최고의 교육을 한 곳에서 볼 수 있는<br />
+        Fingr+를 이용해 보세요.
+      </div>
+      <div class=" left-0 right-0 px-8 pb-4 ">
+        <a
+          href="/"
+          class="text-center w-full rounded-full font-black border border-blue-600 rounded h-12 text-blue-600 flex items-center gap-1 justify-center"
+        >
+          Fingr+ 알아보기</a
+        >
+      </div>
+    </div>
+
+    <div class="border-b">
+      <div class="py-4 mx-4 mt-4 mb-2  text-lg font-black">MY 영상</div>
+      <div class="px-4 pb-4  text-sm text-center text-gray-500 ">
+        이벤트 메뉴에서 영상을 구매·추가해보세요.
+      </div>
+      <div class=" left-0 right-0 px-8 pb-4 ">
+        <a
+          href="/"
+          class="text-center w-full rounded-full font-black border border-blue-600 rounded h-12 text-blue-600 flex items-center gap-1 justify-center"
+        >
+          이벤트 바로가기</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <div class="border-b bg-white">
+    <div class="py-4 mx-4 mt-2 mb-2  text-lg font-black">추천</div>
+    <div class="px-4 pb-4  text-sm text-center text-gray-500 ">
+      좋은 사람과 좋은 회사가 더 많이 연결되도록<br />추천하고, 추천받고,
+      성장하세요
+    </div>
+    <div class=" left-0 right-0 px-8 pb-4 ">
+      <a
+        href="/"
+        class="text-center w-full rounded-full font-black border border-blue-600 rounded h-12 text-blue-600 flex items-center gap-1 justify-center"
+      >
+        추천 시작하기</a
+      >
+    </div>
+  </div>
+</div>
+<div class="pb-14" />
+
+<div
+  class="flex fixed  z-10 bottom-0 bg-white border-t justify-between items-center w-full pr-2-safe h-15"
+>
+  <a href="/" class="flex flex-col items-center w-1/5 py-2 hover text-gray-800"
+    ><span class=""
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-home"
+      >
+        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <polyline points="9 22 9 12 15 12 15 22" /></svg
+      ></span
+    >
+    <span class="text-xs mt-1 font-bold">홈</span>
+  </a><a
+    href="/users"
+    class="flex flex-col items-center w-1/5 py-2 hover text-gray-800"
+    ><span class=""
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-briefcase"
+      >
+        <rect x="2" y="7" width="20" height="14" rx="2" ry="2" />
+        <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+      </svg></span
+    >
+    <span class="text-xs mt-1 font-bold">채용</span>
+  </a><a
+    href="/chats"
+    class="flex flex-col items-center w-1/5 py-2 hover text-gray-800"
+    ><span class=""
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-message-square"
+      >
+        <path
+          d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
+        />
+      </svg></span
+    >
+    <span class="text-xs mt-1 font-bold">커뮤니티</span>
+  </a><a
+    href="/users/mypage"
+    class="flex flex-col items-center w-1/5 py-2 hover text-gray-800"
+    ><span class=""
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-user"
+      >
+        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+        <circle cx="12" cy="7" r="4" /></svg
+      ></span
+    >
+    <span class="text-xs mt-1 font-bold">내 정보</span>
+  </a>
+  <a
+    href="/users/mypage"
+    class="flex flex-col items-center w-1/5 py-2 hover text-gray-800"
+    ><span class=""
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="black"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-smile"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+        <line x1="9" y1="9" x2="9.01" y2="9" />
+        <line x1="15" y1="9" x2="15.01" y2="9" />
+      </svg></span
+    >
+    <span class="text-xs mt-1 font-bold">MY 핑거</span>
+  </a>
+</div>
