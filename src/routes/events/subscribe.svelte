d7ee18ccc9d377e85bf7b012532f83aec5bb6b7d
canonn11 <myungkwanokk@gmail.com>
2022-07-12 22:47:35 +0900

    구독 추천 페이지 구현

---
+++ src/routes/events/subscribe.svelte
@@ -0,0 +1,262 @@
+<div class="fixed w-full z-10 top-0 bg-white flex justify-between border-b">
+  <div class="flex justify-center items-center gap-4 p-4">
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12" />
+        <polyline points="12 19 5 12 12 5" />
+      </svg></a
+    >
+    <div class=" font-black text-center bg-white ml-4">FINGR+</div>
+  </div>
+</div>
+
+<div class="pt-14" />
+
+<div class="relative w-96 h-72">
+  <img
+    class="w-96 h-72 object-cover "
+    src="https://images.unsplash.com/photo-1500048993953-d23a436266cf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDEyfHxwZXJzb258ZW58MHx8fHwxNjQ0NzQxMjkw&ixlib=rb-1.2.1&q=80&w=1080"
+  />
+  <div class="p-4 space-y-2 font-bold text-white absolute bottom-0 mb-2">
+    <div class="text-3xl">FINGR+</div>
+    <div class="text-sm ">
+      구글,페이스북,카카오,라인 등 현업 전문가들의<br /> 인사이트를 무제한으로 업데이트
+      해보세요!
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-between p-4 ">
+  <select class="h-10 px-1 w-20 font-bold  border-none">
+    <option selected>전체</option>
+  </select>
+
+  <button
+    class="flex font-bold text-blue-600 border rounded-full justify-center items-center py-2 px-4"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-search"
+      ><circle cx="11" cy="11" r="8" /><line
+        x1="21"
+        y1="21"
+        x2="16.65"
+        y2="16.65"
+      /></svg
+    >키워드 검색</button
+  >
+</div>
+
+<div class="py-4 mx-4  text-lg  font-black">이번 주! TOP10 콘텐츠</div>
+
+<div class="px-4 pt-4 pb-12">
+  <div class=" gap-2 flex overflow-x-auto">
+    <a href="/" class=" space-y-2 relative ">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1592899677977-9c10ca588bbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDd8fG1vYmlsZXxlbnwwfHx8fDE2NDQ3NDE3MTM&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="w-40 text-xs">Z뱅크 김현수</div>
+      <div class="truncate text-sm">
+        Z뱅크 온보딩 3년 회고<br />(feat. 생생한 실패담)
+      </div>
+      <div class="text-xs text-gray-400 ">채용 : Fingr Con</div>
+      <div class="absolute top-0 right-2">
+        <div
+          class="text-white text-center text-xs bg-blue-600 w-8 h-8 font-bold rounded-md "
+        >
+          TOP<br />10
+        </div>
+      </div>
+    </a>
+    <a href="/" class="space-y-2 relative">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1501162946741-4960f990fdf4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDExfHxtb2JpbGV8ZW58MHx8fHwxNjQ0NzQxNzEz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="w-40 text-xs">Z뱅크 김현수</div>
+      <div class="truncate text-sm">
+        Z뱅크 온보딩 3년 회고<br />(feat. 생생한 실패담)
+      </div>
+      <div class="text-xs text-gray-400 ">채용 : Fingr Con</div>
+      <div class="absolute top-0 right-2">
+        <div class="absolute top-0 right-2">
+          <div
+            class="text-white text-center text-xs bg-blue-600 w-8 h-8 font-bold rounded-md "
+          >
+            TOP<br />10
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" class="space-y-2 relative">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1501162946741-4960f990fdf4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDExfHxtb2JpbGV8ZW58MHx8fHwxNjQ0NzQxNzEz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="w-40 text-xs">Z뱅크 김현수</div>
+      <div class="truncate text-sm">
+        Z뱅크 온보딩 3년 회고<br />(feat. 생생한 실패담)
+      </div>
+      <div class="text-xs text-gray-400 ">채용 : Fingr Con</div>
+      <div class="absolute top-0 right-2">
+        <div class="absolute top-0 right-2">
+          <div
+            class="text-white text-center text-xs bg-blue-600 w-8 h-8 font-bold rounded-md "
+          >
+            TOP<br />10
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="p-4 flex justify-between items-end  mt-2 bg-white">
+  <div class="text-lg font-black flex">HR</div>
+  <a href="/clause" class="text-gray-500  text-sm">전체보기(304)</a>
+</div>
+
+<div class="px-4  pb-8">
+  <div class=" gap-2 flex overflow-x-auto">
+    <a href="/" class="">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDR8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1633249658235-d591f2f2acba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI2fHxyZXdhcmR8ZW58MHx8fHwxNjQ1NDExNzQz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+      <div
+        class=" mt-3 text-sm text-gray-400 font-bold flex items-center gap-1"
+      >
+        #개발 #브랜딩 #커리어고민
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="p-4 flex justify-between items-end  mt-2 bg-white">
+  <div class="text-lg font-black flex">개발</div>
+  <a href="/clause" class="text-gray-500  text-sm">전체보기(304)</a>
+</div>
+
+<div class="px-4  pb-8">
+  <div class=" gap-2 flex overflow-x-auto">
+    <a href="/" class="">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDR8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1633249658235-d591f2f2acba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI2fHxyZXdhcmR8ZW58MHx8fHwxNjQ1NDExNzQz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+      <div
+        class=" mt-3 text-sm text-gray-400 font-bold flex items-center gap-1"
+      >
+        #개발 #브랜딩 #커리어고민
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="p-4 flex justify-between items-end  mt-2 bg-white">
+  <div class="text-lg font-black flex">디자인</div>
+  <a href="/clause" class="text-gray-500  text-sm">전체보기(304)</a>
+</div>
+
+<div class="px-4  pb-8">
+  <div class=" gap-2 flex overflow-x-auto">
+    <a href="/" class="">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDR8fHBlcnNvbnxlbnwwfHx8fDE2NDQ3NDEyOTA&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <img
+        class="rounded-md w-40 h-32 object-cover border border-gray-300"
+        src="https://images.unsplash.com/photo-1633249658235-d591f2f2acba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzQ2NXwwfDF8c2VhcmNofDI2fHxyZXdhcmR8ZW58MHx8fHwxNjQ1NDExNzQz&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+    </a>
+    <a href="/" class="inline-block">
+      <div class="text-xs mt-2 ">최현석</div>
+      <div class="w-40 mt-2 text-sm">월별로 챙겨야 할 노무이슈</div>
+      <div class=" mt-6 text-xs text-gray-400">월별로 챙겨야 할 노무이슈</div>
+      <div
+        class=" mt-3 text-sm text-gray-400 font-bold flex items-center gap-1"
+      >
+        #개발 #브랜딩 #커리어고민
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="pb-20" />
+
+<div class="fixed bottom-0 w-full flex  bg-black p-4 justify-between">
+  <div class="text-white">
+    <div class="">
+      <span class="text-sm">월</span>
+      <span class="text-2xl font-bold">9900</span>
+      <span class="text-sm">원</span>
+    </div>
+    <div class="text-sm">무제한 커리어 콘텐츠</div>
+  </div>
+  <button
+    class="text-white bg-blue-600 rounded-full flex justify-center items-center font-bold px-6"
+    >Fingr+ 구독하기</button
+  >
+</div>
