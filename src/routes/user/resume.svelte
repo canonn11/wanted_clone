44276802cd9e8376ec10e2da529e66704e4c7438
canonn11 <myungkwanokk@gmail.com>
2022-07-12 22:43:40 +0900

    이력서 작성 페이지 구현

---
+++ src/routes/user/resume.svelte
@@ -0,0 +1,324 @@
+<div class="flex h-14  fixed top-0 bg-white w-full  justify-between ">
+  <div class="flex justify-center items-center text-lg gap-8 px-4">
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12" />
+        <polyline points="12 19 5 12 12 5" />
+      </svg></a
+    >
+    <div class=" font-black  text-center">이력서</div>
+  </div>
+  <div class="flex items-center justify-center gap-4">
+    <button
+      ><div
+        class="w-4 h-4 flex justify-center items-center rounded font-bold  border-2 p-2 border-black text-xs "
+      >
+        한
+      </div></button
+    >
+    <button class=" text-sm p-3"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-vertical"
+      >
+        <circle cx="12" cy="12" r="1" />
+        <circle cx="12" cy="5" r="1" />
+        <circle cx="12" cy="19" r="1" />
+      </svg></button
+    >
+  </div>
+</div>
+<div class="pt-14" />
+<div class="grid grid-cols-2 text-center text-xs p-4 font-bold bg-gray-100 ">
+  <div
+    class="flex justify-center items-center text-blue-600 border-r border-gray-400 pr-2"
+  >
+    합격/불합격 단어 가이드
+  </div>
+  <div class="flex  gap-1 items-center justify-center ml-2">
+    <div>내 이력서 단어 체크</div>
+    <button class="relative w-8  px-4 py-1">
+      <div
+        class="absolute top-0 left-0 p-3 border  rounded-full bg-gray-300 drop-shadow-xl   "
+      />
+      <div class="py-2 px-3 bg-gray-400 rounded-full " />
+    </button>
+  </div>
+</div>
+
+<div class="py-4 mx-4 text-3xl font-bold border-b flex gap-2 ">
+  <div>홍길동 1</div>
+  <div class="bg-emerald-400 p-2 rounded-full w-4 h-4 ml-2 mt-1" />
+</div>
+
+<button class="w-full mt-4 flex justify-between items-center h-10">
+  <div class="flex flex-col ">
+    <div class="pl-4 text-left">신입</div>
+  </div>
+  <div class="text-gray-400 pr-4">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-right"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  </div>
+</button>
+
+<div class=" pl-4 text-sm text-left p-2">wqncnkla1137@gmail.com</div>
+<div class=" pl-4 text-sm text-left">+821012345678</div>
+
+<div class="py-4 mx-4 mt-6  text-lg border-b font-black">간단 소개글</div>
+<div class="p-4 h-32">안녕하세요. 신입 웹 개발자입니다.</div>
+
+<div class="flex justify-between py-4 mx-4  text-lg border-b font-black">
+  경력
+  <div class="flex gap-8 text-blue-600">
+    <button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-edit-2"
+      >
+        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+      </svg></button
+    ><button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-plus"
+      >
+        <line x1="12" y1="5" x2="12" y2="19" />
+        <line x1="5" y1="12" x2="19" y2="12" />
+      </svg></button
+    >
+  </div>
+</div>
+
+<button class="w-full flex justify-between items-center h-20">
+  <div class="flex flex-col ">
+    <div class="pl-4 text-left">신입</div>
+
+    <div class=" pl-4 text-gray-400 text-sm text-left">날짜</div>
+  </div>
+  <div class="text-gray-400 pr-4">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-right"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  </div>
+</button>
+
+<div class="flex justify-between py-4 mx-4  text-lg border-b font-black">
+  학력
+  <div class="flex gap-8 text-blue-600">
+    <button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-edit-2"
+      >
+        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+      </svg></button
+    ><button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-plus"
+      >
+        <line x1="12" y1="5" x2="12" y2="19" />
+        <line x1="5" y1="12" x2="19" y2="12" />
+      </svg></button
+    >
+  </div>
+</div>
+
+<button class="w-full flex justify-between items-center h-20">
+  <div class="flex flex-col ">
+    <div class="pl-4 text-left">123대학교</div>
+
+    <div class=" pl-4 text-gray-400 text-sm text-left">날짜</div>
+  </div>
+  <div class="text-gray-400 pr-4">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-right"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  </div>
+</button>
+
+<div class="py-4 mx-4 mt-6  text-lg border-b font-black flex justify-between ">
+  <div>수상 및 기타</div>
+  <button class="text-blue-600"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-plus"
+    >
+      <line x1="12" y1="5" x2="12" y2="19" />
+      <line x1="5" y1="12" x2="19" y2="12" />
+    </svg></button
+  >
+</div>
+<div class="p-4 h-32 text-gray-400 ">
+  직무와 관련한 주요 수상, 자격증, 활동 등을 <br />입력하세요.
+</div>
+
+<div class="py-4 mx-4 mt-6  text-lg border-b font-black flex justify-between ">
+  <div>외국어</div>
+  <button class="text-blue-600"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-plus"
+    >
+      <line x1="12" y1="5" x2="12" y2="19" />
+      <line x1="5" y1="12" x2="19" y2="12" />
+    </svg></button
+  >
+</div>
+<div class="p-4 h-32 text-gray-400">
+  업무에 활용 가능한 외국어 능력을 입력하세요.
+</div>
+
+<div class="py-4 mx-4 mt-6  text-lg border-b font-black flex justify-between ">
+  <div>링크</div>
+  <button class="text-blue-600"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-plus"
+    >
+      <line x1="12" y1="5" x2="12" y2="19" />
+      <line x1="5" y1="12" x2="19" y2="12" />
+    </svg></button
+  >
+</div>
+<div class="p-4 h-32 text-gray-400">
+  직무 경력과 관련하여 참고할 수 있는 링크를 입력하세요.
+</div>
+
+<div class="flex items-center border-t border-b  py-4 px-2">
+  <div class="flex flex-col  w-full text-xs font-black ">
+    <div class="ml-3">87</div>
+    <div class="flex items-center  gap-2 ">
+      <div class="relative w-36 h-2 bg-gray-200 rounded-full ">
+        <div class="absolute h-2 w-2 bg-black rounded" />
+        <div class="ml-1 absolute h-2 w-4 bg-black rounded" />
+        <div class="h-2  border-l border-dashed border-blue-400 ml-8" />
+      </div>
+
+      <div class="">💪</div>
+    </div>
+    <div class="flex gap-4 text-xs font-black">
+      <div class="text-gray-400">0</div>
+      <div class="">400</div>
+    </div>
+  </div>
+  <div class="text-xs w-full px-2 justify-center items-center ">
+    400자 이상 작성하시면<br /> 서류합격률을 예측해드려요!
+  </div>
+</div>
+<div class="pb-14" />
+
+<div
+  class="fixed bottom-0 w-full bg-white border-t  h-14 flex items-center justify-center border-b bg-white"
+>
+  <button class="font-bold text-blue-600 ">작성 완료하기</button>
+</div>
