6af231c4c3f7ca1aa22fb5ae74a99f56f6f60f82
canonn11 <myungkwanokk@gmail.com>
2022-07-12 22:46:02 +0900

    친구 페이지 구현

---
+++ src/routes/user/friend.svelte
@@ -0,0 +1,246 @@
+<div class="flex h-14  bg-white w-full  justify-between ">
+  <div class="flex justify-center items-center text-lg gap-8 px-4">
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12" />
+        <polyline points="12 19 5 12 12 5" />
+      </svg></a
+    >
+    <div class=" font-black  text-center">추천</div>
+  </div>
+  <div class="flex items-center justify-center gap-4">
+    <button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bell"
+        ><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" /><path
+          d="M13.73 21a2 2 0 0 1-3.46 0"
+        /></svg
+      ></button
+    >
+    <button class=" text-sm p-3"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-settings"
+        ><circle cx="12" cy="12" r="3" /><path
+          d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
+        /></svg
+      ></button
+    >
+  </div>
+</div>
+
+<div class="grid grid-cols-3 border-b">
+  <button
+    class="w-full border-b  border-blue-600 text-blue-600 text-center px-2 py-3 "
+    >추천하기</button
+  >
+  <button class="w-full whitespace-pre text-gray-600 text-center px-2 py-3"
+    >내가 받은 추천</button
+  >
+  <button class="w-full text-gray-600 text-center px-2 py-3"
+    >내가 한 추천</button
+  >
+</div>
+
+<div class="flex items-center gap-2 w-full border-b p-3">
+  <div class="text-gray-400">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-search"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+  </div>
+  <div class=" text-gray-400">친구 검색</div>
+</div>
+<div class="bg-gray-200">
+  <div class="pt-2" />
+  <div class="w-full flex bg-white gap-4 items-center py-6 pl-4 border ">
+    <div class="relative flex justify-end items-center w-16 h-10">
+      <div
+        class="w-6 h-6 rounded-full bg-gray-100 flex justify-center items-center text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-plus"
+          ><line x1="12" y1="5" x2="12" y2="19" /><line
+            x1="5"
+            y1="12"
+            x2="19"
+            y2="12"
+          /></svg
+        >
+      </div>
+      <div
+        class="absolute  left-5 w-6 h-6 rounded-full bg-blue-500 flex justify-center items-center text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-user"
+          ><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" /><circle
+            cx="12"
+            cy="7"
+            r="4"
+          /></svg
+        >
+      </div>
+      <div
+        class=" absolute left-0 w-6 h-6 rounded-full bg-blue-600 flex justify-center items-center text-white "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-facebook"
+          ><path
+            d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+          /></svg
+        >
+      </div>
+    </div>
+    <div class="text-sm">
+      간단하게 네트워크에 연결하고<br />지인들의 추천을 받아보세요.
+    </div>
+    <div class="pl-6">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-right"
+        ><polyline points="9 18 15 12 9 6" /></svg
+      >
+    </div>
+  </div>
+  <div class="pt-2" />
+
+  <div class="flex items-center justify-between bg-white border-b">
+    <div class="flex gap-3 items-center justify-center p-4">
+      <div
+        class="w-6 h-6 bg-blue-500 flex items-center justify-center text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-mail"
+          ><path
+            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+          /><polyline points="22,6 12,13 2,6" /></svg
+        >
+      </div>
+      <div class="">주소록 추천</div>
+    </div>
+    <div class="p-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-right"
+        ><polyline points="9 18 15 12 9 6" /></svg
+      >
+    </div>
+  </div>
+
+  <div class="flex items-center justify-center text-gray-400 bg-white pt-7 ">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="72"
+      height="72"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-frown"
+      ><circle cx="12" cy="12" r="10" /><path
+        d="M16 16s-1.5-2-4-2-4 2-4 2"
+      /><line x1="9" y1="9" x2="9.01" y2="9" /><line
+        x1="15"
+        y1="9"
+        x2="15.01"
+        y2="9"
+      /></svg
+    >
+  </div>
+  <div class="text-gray-400 text-center text-sm bg-white pt-4 ">
+    추천 가능한 지인이 없습니다.
+  </div>
+</div>
